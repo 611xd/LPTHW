baa47da904f9f2c65ddeb52465a572b564c48cc3
LUK YAN YAN <lukyanyan@LUKdeMacBook-Pro.local>
2016-10-25 16:24:44 +0200

    set up directories and ex 1-14

---
+++ EX02/ex2.py
@@ -0,0 +1,8 @@
+#diu nei lo mei la diudiudiuiu
+
+print "I could have code like this." #and fuck you education
+
+#fuck you education
+# print "This won't run."
+
+print "This will run."
---
+++ EX05/ex5.py
@@ -0,0 +1,18 @@
+my_name = "Luk Yan Yan"
+my_age = 20
+my_height = 157.5
+my_weight = 53
+my_eyes = 'Brown'
+my_teeth = 'Kind of yellow'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "She's %d cm tall." % my_height
+print "She's %d kg heavy." % my_weight
+print "Actually that's not too heavy."
+print "She's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "Her teeth are usually %s depending on the tea." % my_teeth
+
+
+print "if I add %d, %d, and %d I get %d." % (
+my_age, my_height, my_weight, my_age + my_height + my_weight)
